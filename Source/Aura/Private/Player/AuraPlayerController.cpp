e2c53e831a8039b03327df077ec1fdacfd1d4104
Feinzenz <vinzenz.landl@klangfestival.at>
2026-07-16 17:48:05 +0200

    Movement Input

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,6 +3,8 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputAction.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -25,4 +27,29 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>(); //Input Value
+	const FRotator Rotation = GetControlRotation(); //Current Camera Rotation
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f); //Current Camera Yaw
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X); //Converts Camera Forward to World Vector Forward
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y); //Converts Camera Forward to World Vector Right
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
 }
