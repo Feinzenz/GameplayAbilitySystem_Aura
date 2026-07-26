8cfc18d58eed2f835e83d74b69ee75bddc87bab0
Feinzenz <vinzenz.landl@klangfestival.at>
2026-07-26 19:44:12 +0200

    Section 7.57

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -18,8 +18,8 @@ public:
 	AAuraCharacter();
 	virtual void PossessedBy(AController* NewController) override;	
 	virtual void OnRep_PlayerState() override;
-	
-private:
-	void InitAbilityActorInfo();
+
+protected:
+	virtual void InitAbilityActorInfo() override;
 	
 };
