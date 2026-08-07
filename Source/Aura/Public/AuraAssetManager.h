836c176037d8493bec6912b866cfd21e0fd1e660
Feinzenz <vinzenz.landl@klangfestival.at>
2026-08-07 18:26:12 +0200

    Section 8.88 baeh

---
+++ Source/Aura/Public/AuraAssetManager.h
@@ -0,0 +1,21 @@
+// Copyright Vinzenz Landl
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "AuraAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+public:
+	static UAuraAssetManager& Get();
+	
+protected:
+	virtual void StartInitialLoading() override;
+};
