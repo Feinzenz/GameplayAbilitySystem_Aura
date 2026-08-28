388ad3e4aa5126915ca530ffc8709927c0c3adc6
Feinzenz <vinzenz.landl@klangfestival.at>
2026-08-28 21:07:58 +0200

    Section 12 Complete

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Vinzenz Landl
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(const ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
